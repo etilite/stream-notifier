fbc37892755f498df37dc4499615a817131ee40b
Alexander <good.cannibal@gmail.com>
2023-12-18 01:03:15 +0300

    chore: extend interfaces

--- internal/domain/interface.go
+++ internal/domain/interface.go
@@ -1,12 +1,32 @@
 package domain
 
-import "context"
+import (
+	"context"
 
-type Sender interface {
-	Send(follower Follower, dto CheckResultDTO) (string, error)
+	"github.com/etilite/stream-notifier/internal/domain/dto"
+)
+
+type StreamChecker interface {
+	Check(in <-chan string) <-chan dto.CheckResultDTO
 }
 
-type Runner interface {
-	// todo add error return
-	Run(ctx context.Context)
+type Splitter interface {
+	Split(in <-chan dto.CheckResultDTO) (<-chan dto.PersonalCheckResultDTO, <-chan *Notification)
+}
+
+type OnlineResultsHandler interface {
+	Handle(ctx context.Context, in <-chan dto.PersonalCheckResultDTO)
+}
+
+type OfflineResultsHandler interface {
+	Handle(ctx context.Context, in <-chan Notification)
+}
+
+type Sender interface {
+	Send(follower Follower, dto dto.CheckResultDTO) (string, error)
+}
+
+type Doer interface {
+	// todo add error return?
+	Do(ctx context.Context)
 }
